2796c0edfb9692184794f3c24a171708e9d8ed14
George Gomes <georgecarlos@live.com>
2024-10-03 08:48:55 +0100

    included new fields to prudctmodel

--- src/models/Product.js
+++ src/models/Product.js
@@ -11,24 +11,56 @@ export default class Product extends Model{
           allowNull: false,
           comment: "Product id to the Store"
         },
+
+        isProductOrServices:{
+          type: Sequelize.VIRTUAL,
+          allowNull: false,
+          validate:{
+            isIn: ["product", "services"],
+            msg: "Its mandatory to awnser this question."
+          },
+          comment:"A mandatory field to especify is a product or a service."
+        },
+
+        hasSize: {
+          type: Sequelize.VIRTUAL,
+          allowNull: false,
+          validate:{
+            isIn: ["yes", "no"],
+            msg: "Its mandatory to awnser this question."
+          }
+        },
+
+        hasColor: {
+          type: Sequelize.VIRTUAL,
+          allowNull: false,
+          validate:{
+            isIn: ["yes", "no"],
+            msg: "Its mandatory to awnser this question."
+          }
+        },
+
         ean:{
           type: Sequelize.INTEGER,
           allowNull: false,
           unique: true,
-          comment: "Barcode product",
           validate:{
             isInt: true,
             msg: "The barcode must to be a number"
           },
+          comment: "Barcode product",
         },
+
         name:{
           type: Sequelize.STRING,
           allowNull: false,
           validate:{
             len:[[Op.gte]:3],
             msg:"The name size must be at least 3 characters"
-          }
+          },
+          comment: "The product name"
         },
+
         price:{
           type: Sequelize.DECIMAL(2),
           allowNull: false,
@@ -36,24 +68,49 @@ export default class Product extends Model{
             isDecimal: true,
             is: [[Op.gte]: 0],
             msg: "The price must be a Decimal value and greater than zero"
-          }
+          },
+          comment: "product price"
         },
+
         description:{
           type: Sequelize.TEXT,
           allowNull: false,
           validate:{
             notNull: true,
             msg: "All products must to have a description."
-          }
+          },
+          comment: "A text with the product description"
         },
-        /*
-        Incluir atributos que pdem ou nao existir para o produto, por exemplo
-        se o produto tem ou nao uma cor a ser especificada
-        se o produto tem ou nao dimensoes a serem especificadas
-        se o produto tem ou nao classificação energetica a ser especificada
-        ... etc
-        */
+
+        stock:{
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          validate:{
+            notNull: true,
+            msg: "Its mandatory to enter with the quantity"
+          },
+          comment: "Initial quantity of the product"
+        },
+
+        height: {
+          type: Sequelize.DECIMAL(2),
+          allowNull: true,
+          comment: "Height value in kg"
+        },
+
+        whidth:{
+          type: Sequelize.DECIMAL(2),
+          allowNull: true,
+          comment: "Width value in kg"
+        },
+
+        Wheight: {
+          type: Sequelize.DECIMAL(3),
+          allowNull: true,
+          comment: "The product wheight in Kg."
+        },
       },
+
       {
         sequelize,
         tableName: "Product",
@@ -61,7 +118,9 @@ export default class Product extends Model{
         underscored: true,
       },
     );
+
     return this;
+
   }
 
   static associate(models){
